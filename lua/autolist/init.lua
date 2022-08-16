eed89ea1d81a3ea1baf89de09decfa819221e39e
Dean <gao.dean@hotmail.com>
2022-08-16 16:24:31 +1000

    after minimize

--- lua/autolist/init.lua
+++ lua/autolist/init.lua
@@ -1,9 +1,5 @@
 local M = {}
 
-M.default_config = {
-	create_cr_mapping = true
-}
-
 M.list = function()
 	local preceding_line = vim.fn.getline(vim.fn.line(".") - 1)
 	if preceding_line:match("^%s*%d+%.%s.") then
@@ -16,23 +12,6 @@ M.list = function()
 	elseif preceding_line:match("^%s*[-+*]") and #preceding_line:match("[-+*].*") == 1 then
 		vim.fn.setline(vim.fn.line(".") - 1, "")
 		vim.fn.setline(".", "")
-	end
-end
-
-M.setup = function(config)
-	vim.validate({ config = { config, 'table', true } })
-	config = vim.tbl_deep_extend('force', M.default_config, config or {})
-	create_cr_mapping = config.create_cr_mapping
-	config = setup_config(config)
-	apply_config(config)
-end
-
-local function setup_config(config)
-end
-
-local function apply_config(config)
-	if config.create_cr_mapping then
-		vim.api.nvim_set_keymap('i', '<cr>', [[<cr><cmd>lua require("autolist").list()<cr>]], { noremap = true, silent = true })
 	end
 end
 
