13a948b5cf93cc6a2b559e704a46106d1f3688de
Dean <gao.dean@hotmail.com>
2022-08-29 20:50:01 +1000

    refactor: update on waterfall, make a decrease function that decreases any ordered list

--- lua/autolist/auto.lua
+++ lua/autolist/auto.lua
@@ -54,4 +54,44 @@ function M.new()
 	end
 end
 
+-- called it waterfall because the ordered list entries after {ptrline}
+-- that belongs to the same list has {rise} added to it.
+local function waterfall(ptrline, rise, override_line)
+	local cur_indent_lvl
+	if override_line then
+		cur_indent_lvl = utils.get_indent_lvl(override_line)
+	else
+		cur_indent_lvl = utils.get_indent_lvl(fn.getline(ptrline))
+	end
+	-- waterfall only needs to affect after current line
+	ptrline = ptrline + 1
+	local eval_ptrline = fn.getline(ptrline)
+	local val = utils.is_ordered(eval_ptrline, rise, config.list_types)
+	local ptr_indent_lvl = utils.get_indent_lvl(eval_ptrline)
+
+	-- waterfall only acts on ordered lists
+	-- if its smaller than current indent, the list is "out of scope"
+	-- it can be bigger because it could be unordered but parent list ordered
+
+	-- loop through lines in current list and incement/decrement ordered lists
+	while ptr_indent_lvl > cur_indent_lvl
+		or val ~= nil
+	do
+		-- a stricter check
+		if ptr_indent_lvl == cur_indent_lvl
+			and val ~= nil
+		then
+			-- if val isn't nil, it has a value that was increment/decrement
+			fn.setline(ptrline, val)
+		end
+		ptrline = ptrline + 1
+		if ptrline > fn.line('$') then
+			return
+		end
+		eval_ptrline = fn.getline(ptrline)
+		val = utils.is_ordered(eval_ptrline, config.list_types)
+		ptr_indent_lvl = utils.get_indent_lvl(eval_ptrline)
+	end
+end
+
 return M
--- lua/autolist/utils.lua
+++ lua/autolist/utils.lua
@@ -1,8 +1,5 @@
-local config = require("autolist.config")
-
 local pat_num = "%d+"
 local pat_char = "%a"
-
 local fn = vim.fn
 
 local M = {}
@@ -29,6 +26,27 @@ function M.increment(entry)
 	return entry
 end
 
+function M.decrement(entry)
+	local digit = entry:match(pat_num)
+	local char = entry:match(pat_char)
+	-- if its an ordered list
+	if digit then
+		return entry:gsub(digit, digit - 1, 1)
+	-- if it's an ascii list
+	elseif char then
+		local byteform = char:byte() - 1
+		-- if bigger than lowercase z wrap to upper A and vice versa
+		if byteform < 65 then
+			byteform = 122
+		elseif byteform > 90 and byteform < 97
+			then byteform = 90
+		end
+		return entry:gsub(char, string.char(byteform), 1)
+	end
+	-- return original if anything else
+	return entry
+end
+
 -- set current line to {new_line} and set cursor to end of line
 function M.set_current_line(new_line)
 	fn.setline(".", new_line)
@@ -39,18 +57,26 @@ function M.set_current_line(new_line)
 end
 
 --is ordered list
-local function is_ordered(entry)
+function M.is_ordered(entry, rise, list_types)
 	-- increment only acts on incrementable (ordered) lists
-	if M.increment(entry) == entry then
-		return false
+	if is_list(entry, list_types) then
+		local newval
+		if rise > 0 then
+			newval = M.increment(entry)
+		else
+			newval = M.decrement(entry)
+		end
+		-- if increment changed {entry} it is changable thus ordered
+		if newval ~= entry then
+			return newval
+		end
 	end
-	-- if increment changed {entry} (incrementable)
-	return true
+	return nil
 end
 
 -- is a list
-local function is_list(entry)
-	for _, pat in ipairs(config.list_types) do
+function M.is_list(entry, list_types)
+	for _, pat in ipairs(list_types) do
 		local _, nsubs = entry:gsub("^%s*(" .. pat .. "%s?).*$", "%1", 1)
 		-- if replaced something
 		if nsubs > 0 then
@@ -60,42 +86,9 @@ local function is_list(entry)
 	return false
 end
 
-local function get_indent_lvl(entry)
-	-- returns the number of tabs/spaces before a character
+-- returns the number of tabs/spaces before a character
+function M.get_indent_lvl(entry)
 	return #(entry:match("^%s*"))
 end
-
--- called it waterfall because the ordered list entries after {ptrline}
--- that belongs to the same list has {rise} added to it.
-local function waterfall(ptrline, rise, override_line)
-	local cur_indent_lvl
-	if override_line then
-		cur_indent_lvl = get_indent_lvl(override_line)
-	else
-		cur_indent_lvl = get_indent_lvl(fn.getline(ptrline))
-	end
-	-- waterfall only needs to affect after current line
-	ptrline = ptrline + 1
-	local eval_ptrline = fn.getline(ptrline)
-	-- waterfall only acts on ordered lists
-	-- if its smaller than current indent, the list is "out of scope"
-	-- it can be bigger because it could be unordered but parent list ordered
-	while is_ordered(eval_ptrline)
-		or get_indent_lvl(eval_ptrline) > cur_indent_lvl
-	do
-		if indent_level(eval_ptrline) == cur_indent_lvl
-			and is_ordered(eval_ptrline)
-		then
-			-- increment it
-			fn.setline(ptrline, M.increment(eval_ptrline))
-		end
-		ptrline = ptrline + 1
-		if ptrline > fn.line('$') then
-			return
-		end
-		eval_ptrline = fn.getline(ptrline)
-	end
-end
-
 
 return M
