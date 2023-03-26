6fc6e376d7f3e2a69a3c364294a14d3c620075e2
Dean <gao.dean@hotmail.com>
2023-03-26 19:31:12 +1100

    config: remove list_cap config option

--- lua/autolist/config.lua
+++ lua/autolist/config.lua
@@ -1,45 +1,53 @@
 local default_config = {
-	enabled = true,
-	list_cap = 50,
-	colon = {
-		indent_raw = true,
-		indent = true,
-		preferred = "-",
-	},
-	invert = {
-		indent = false,
-		toggles_checkbox = true,
-		ul_marker = "-",
-		ol_incrementable = "1",
-	},
-	lists = {
-		markdown = {
-			"unordered",
-			"digit",
-			"ascii",
-			"roman",
-		},
-		text = {
-			"unordered",
-			"digit",
-			"ascii",
-			"roman",
-		},
-		tex = { "latex_item" },
-		plaintex = { "latex_item" },
-	},
-	list_patterns = {
-		unordered = "[-+*]", -- - + *
-		digit = "%d+[.)]", -- 1. 2. 3.
-		ascii = "%l[.)]", -- a) b) c)
-		roman = "%u*[.)]", -- I) II) III)
-		latex_item = "\\item",
-	},
-	checkbox = {
-		left = "%[",
-		right = "%]",
-		fill = "x",
-	},
+  enabled = true,
+  colon = { -- if a line ends in a colon
+    indent = true, -- if in list and line ends in `:` then create list
+    indent_raw = true, -- above, but doesn't need to be in a list to work
+    preferred = "-", -- what the new list starts with (can be `1.` etc)
+  },
+  invert = { -- Inverts the list type (ol -> ul, ul -> ol, [ ] -> [x])
+    indent = false, -- when on top level list, pressing invert inverts the list and indents it
+    toggles_checkbox = true, -- if pressing invert toggles checkbox
+    ul_marker = "-", -- when from ordered list to unordered, set marker to whatever this is
+    ol_incrementable = "1", -- same thing above but for ordered
+  },
+  lists = { -- configures list behavio
+    -- Each key in lists represents a filetype.
+    -- The value is a table of all the list patterns that the filetype implements.
+    -- See how to define your custom list below
+    -- You can see a few preloaded options in the default configuration such as "unordered" and "digit"
+    -- of which the full set you can find in the config.list_patterns
+    -- You must put the file name for the filetype, not the file extension
+    -- To get the "file name", it is just =:set filetype?= or =:se ft?=.
+    markdown = {
+      "unordered",
+      "digit",
+      "ascii", -- specifies activate the ascii list type for markdown files
+      "roman", -- see below on the list types
+    },
+    text = {
+      "unordered",
+      "digit",
+      "ascii",
+      "roman",
+    },
+    tex = { "latex_item" },
+    plaintex = { "latex_item" },
+  },
+  list_patterns = { -- custom list types: see README -> Configuration -> defining custom lists
+    unordered = "[-+*]", -- - + *
+    digit = "%d+[.)]", -- 1. 2. 3.
+    ascii = "%a[.)]", -- a) b) c)
+    roman = "%u*[.)]", -- I. II. III.
+    latex_item = "\\item",
+  },
+  checkbox = {
+    left = "%[", -- the left checkbox delimiter (you could change to "%(" for brackets)
+    right = "%]", -- the right checkbox delim (same customisation as above)
+    fill = "x", -- if you do the above two customisations, your checkbox could be (x) instead of [x]
+  },
+
+  -- this is all based on lua patterns, see "Defining custom lists" for a nice article to learn them
 }
 
 local function get_preloaded_pattern(config, pre)
@@ -67,6 +75,7 @@ M.update = function(opts)
 	end
 
 	-- options that are hidden from config options but accessible by the scripts
+	M.list_cap = 50
 	M.tabstop = vim.opt.tabstop:get()
 	if vim.opt.expandtab:get() then
 		local pattern = ""
