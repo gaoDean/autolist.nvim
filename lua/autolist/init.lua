adfc2e012ca220ba2a57f4035a0987966bcb9031
Dean <gao.dean@hotmail.com>
2023-04-21 12:47:22 +1000

    usage: add vim PascalCase commands

--- lua/autolist/init.lua
+++ lua/autolist/init.lua
@@ -2,10 +2,21 @@ local config = require("autolist.config")
 
 local M = {}
 
+local function snake_to_pascal_case(snake_str)
+  snake_str = snake_str:gsub("^(%l)", function (l) return l:upper() end, 1)
+  return snake_str:gsub("_(%l)", function (l) return l:upper() end)
+end
+
 M.setup = function(opts)
 	config.update(opts)
 	for k, v in pairs(require("autolist.auto")) do
 		M[k] = v
+        vim.cmd("command! "
+                .. "Autolist"
+                .. snake_to_pascal_case(k)
+                .. " lua require('autolist')."
+                .. k
+                .. "()")
 	end
 end
 
